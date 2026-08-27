4c2933f07b0b9255d20b0c017a110376cfd7fb09
onintsoatahiana <onintsoa.tahiana@gmail.com>
2026-08-27 18:45:41 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,11 @@
-const nextConfig={reactStrictMode:true}; export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
